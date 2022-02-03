43059136442a908d018555ceb1e415353f6dfda2
Vera Rojman <vrojman@protonmail.com>
2022-02-03 14:37:40 +0100

    Basic quote tag

---
+++ lib/jekyll-plugin-platoniq-journal/quote_tag.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Jekyll
+  class QuoteBlockTag < Liquid::Block
+    def initialize(tag_name, input, tokens)
+      super
+      @input = input
+    end
+
+    def render(_context)
+      text = super
+
+      jdata = if !@input.nil? && !@input.empty?
+                JSON.parse(@input)
+              else
+                JSON.parse({ :example => "123" })
+              end
+
+      output = []
+
+      output << %(<figure class="pj-quote">)
+      output << <<~QUOTE
+        <blockquote>
+          #{Kramdown::Document.new(text).to_html if text}
+        </blockquote>
+        <figcaption>
+          &mdash; #{jdata["author"]}
+        </figcaption>
+      QUOTE
+      output << %(</figure>)
+
+      output.join
+    end
+  end
+end
+
+Liquid::Template.register_tag("quote", Jekyll::QuoteBlockTag)
