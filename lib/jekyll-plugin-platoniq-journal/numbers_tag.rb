40a3ef7b5dfe4b5dd3fbf8c2f7e59422baf2ea55
Vera Rojman <vrojman@protonmail.com>
2022-02-17 16:27:29 +0100

    Add icons to numbers tag

--- lib/jekyll-plugin-platoniq-journal/numbers_tag.rb
+++ lib/jekyll-plugin-platoniq-journal/numbers_tag.rb
@@ -2,17 +2,26 @@
 
 module Jekyll
   class NumbersTag < Liquid::Tag
+    include JekyllPluginPlatoniqJournal::Base
+    include JekyllPluginPlatoniqJournal::IncludesFile
+
     def initialize(tag_name, input, tokens)
       super
       @input = input
     end
 
-    def render(_context)
-      jdata = if !@input.nil? && !@input.empty?
-                JSON.parse(@input)
-              else
-                JSON.parse({ :example => "123" })
-              end
+    def render(context)
+      @context = context
+      @site = site
+
+      @site.inclusions[icon_file_path] ||= locate_include_file(icon_file_path)
+
+      add_include_to_dependency(inclusion, context) if site.config["incremental"]
+
+      @icon = nil
+      context.stack do
+        @icon = inclusion.render(context)
+      end
 
       output = []
 
@@ -24,6 +33,7 @@ module Jekyll
               #{value}
             </strong>
             <span class="pj-number--description">
+              <span class="pj-number--icon">#{@icon}</span>
               #{key}
             </span>
           </li>
@@ -32,6 +42,24 @@ module Jekyll
       output << %(</ul>)
 
       output.join
+    end
+
+    private
+
+    def jdata
+      @jdata ||= if !@input.nil? && !@input.empty?
+                   JSON.parse(@input)
+                 else
+                   JSON.parse({ :example => "123" })
+                 end
+    end
+
+    def icon_file_path
+      @icon_file_path ||= if !jdata.nil? && jdata["icon"]
+                            jdata["icon"]
+                          else
+                            "svg/icon-arrow_leftup.liquid"
+                          end
     end
   end
 end
