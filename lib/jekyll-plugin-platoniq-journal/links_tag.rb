e905e279a4ee388be7cdac1e1053a533d22b983b
Vera Rojman <vrojman@protonmail.com>
2022-02-17 16:13:21 +0100

    Update links tag with new modules

--- lib/jekyll-plugin-platoniq-journal/links_tag.rb
+++ lib/jekyll-plugin-platoniq-journal/links_tag.rb
@@ -2,37 +2,75 @@
 
 module Jekyll
   class LinksTag < Liquid::Tag
+    include JekyllPluginPlatoniqJournal::Base
+    include JekyllPluginPlatoniqJournal::IncludesFile
+
     def initialize(tag_name, input, tokens)
       super
       @input = input
     end
 
-    def render(_context)
-      jdata = if !@input.nil? && !@input.empty?
-                JSON.parse(@input)
-              else
-                JSON.parse({ :example => "123" })
-              end
+    def render(context)
+      @context = context
+      @site = site
 
+      @site.inclusions[icon_file_path] ||= locate_include_file(icon_file_path)
+
+      add_include_to_dependency(inclusion, context) if site.config["incremental"]
+
+      @icon = nil
+      context.stack do
+        @icon = inclusion.render(context)
+      end
+
+      render_all
+    end
+
+    private
+
+    def jdata
+      @jdata ||= JSON.parse(@input) if !@input.nil? && !@input.empty?
+    end
+
+    def icon_file_path
+      @icon_file_path ||= if !jdata.nil? && jdata["icon"]
+                            jdata["icon"]
+                          else
+                            "svg/icon-link.liquid"
+                          end
+    end
+
+    def render_all
       output = []
 
       output << %(<section class="pj-links">)
-      output << %(<h3>#{jdata["title"]}</h3>)
-      output << %(<ul class="llistat-links">)
-      output << jdata["items"].map { |item| render_item(item) }
-      output << %(</ul></section>)
+      output << render_title unless jdata["title"].empty?
+      output << render_list if jdata["items"].any?
+      output << %(</section>)
 
       output.join
     end
 
-    private
+    def render_title
+      %(<h3>#{jdata["title"]}</h3>)
+    end
+
+    def render_list
+      output = []
+
+      output << %(<ul class="llistat-links">)
+      output << jdata["items"].map { |item| render_item(item) }
+      output << %(</ul>)
+
+      output.join
+    end
 
     def render_item(item)
       <<~LINK
         <li>
           <a href="#{item["url"]}" class="link link--extern">
             <span class="link__icon link__icon--left">
-              <img src="img/ico/links--primary.svg" alt="#{item["title"]}">
+              #{@icon}
             </span>
             <span class="link__label">
               #{item["title"]}
